0282dcaa6d3016dd18fb07678a359018d50575c2
luben <lubenfrank48@gmail.com>
2019-08-02 13:02:41 +0530

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import SingleRecipe from "./pages/SingleRecipe";
+import Default from "./pages/Default";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
-  return <div className="App">hello</div>;
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <main>
+          {/* navbar */}
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/recipes" exact component={Recipes} />
+            <Route path="/recipes/:id" exact={true} component={SingleRecipe} />
+            <Route component={Default} />
+          </Switch>
+        </main>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/SingleRecipe.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Reciper extends Component {
+  render() {
+    return <h1>Hello from SingleRecipes</h1>;
+  }
+}
