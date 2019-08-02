370f2ed182b4c4609f27da9159aa211cd32002b1
luben <lubenfrank48@gmail.com>
2019-08-02 14:59:05 +0530

    recipe page structure

---
+++ src/components/Recipe.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class RecipeList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Recipes</h1>
+      </div>
+    );
+  }
+}
---
+++ src/components/Search.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Reciper extends Component {
+  render() {
+    return <div>list</div>;
+  }
+}
