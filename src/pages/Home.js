f4170e6ca04aaa4387958c820cb7f5e5ce36192f
luben <lubenfrank48@gmail.com>
2019-08-02 13:49:47 +0530

    home page done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 export default class Reciper extends Component {
   render() {
-    return <h1>Hello from home</h1>;
+    return (
+      <Header title="amazing recipes">
+        <Link to="recipes" className="text-uppercase btn-secondary btn-lg mt-3">
+          search recipes
+        </Link>
+      </Header>
+    );
   }
 }
